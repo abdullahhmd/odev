271c472cab951253df444c8ac2a3504ec05cbafd
abdullahhmd <apo.hamidi@gmail.com>
2018-03-04 01:07:50 +0300

    Add files via upload

---
+++ odev3.py
@@ -0,0 +1,7 @@
+def ciro () :
+    satis_mik=int(input("satis miktarini gir"))
+    birim_satis_fiyat=int(input("birim satıs fiyatını gir"))
+    toplam_calisan_sayisi=int(25)
+    ciro=satis_mik*birim_satis_fiyat
+    adambasi_ciro=ciro/toplam_calisan_sayisi
+    return adambasi_ciro

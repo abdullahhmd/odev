48ecc6eefaa1c638edfd8dfd57d3eccaaab1318e
abdullahhmd <apo.hamidi@gmail.com>
2018-03-16 15:58:39 +0300

    ödev2

---
+++ odev2.15.03.py
@@ -0,0 +1,19 @@
+x=170
+y=50
+def ayılı():
+    global calismasure
+    calismasure=x/y
+    return calismasure
+def byılı():
+    w=x+50
+    e=y+20
+    global calismasure
+    calismasure=w/e
+    return calismasure
+a=ayılı()
+b=byılı()
+
+fark=a-b
+print("2016 2017 arasındaki fark=",fark)
+    
+    
